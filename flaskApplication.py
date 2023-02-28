df8f4f70032f237f6a95bc5b4c21237b7579c3e5
sajib-rana <sajibrana444@gmail.com>
2023-02-28 20:35:13 +0600

    creat an router for operation

--- flaskApplication.py
+++ flaskApplication.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
- return '<h1>Hello World!</h1>'
+@app.route('/math')
+def math():
+    operations = ['add', 'subtract', 'multiply', 'divide', 'modulus']
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
 
 if __name__ == '__main__':
     app.run(debug=True)
