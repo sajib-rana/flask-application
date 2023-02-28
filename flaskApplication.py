1ad99afc6d111ba03ee742cfe95a404812f4bb43
sajib-rana <sajibrana444@gmail.com>
2023-02-28 20:37:17 +0600

    using python flask loop an calculator

--- flaskApplication.py
+++ flaskApplication.py
@@ -6,6 +6,22 @@ def math():
     operations = ['add', 'subtract', 'multiply', 'divide', 'modulus']
     a = int(request.args.get('a'))
     b = int(request.args.get('b'))
-
+    results = []
+    for op in operations:
+        if op == 'add':
+            result = a + b
+        elif op == 'subtract':
+            result = a - b
+        elif op == 'multiply':
+            result = a * b
+        elif op == 'divide':
+            result = a / b
+        elif op == 'modulus':
+            result = a % b
+        else:
+            result = None
+        results.append((op, result))
+    return str(results)
+    
 if __name__ == '__main__':
     app.run(debug=True)
