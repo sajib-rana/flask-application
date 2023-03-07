c8d0944cc0c99bf4e5b2fb3701913007401d4570
sajib-rana <sajibrana444@gmail.com>
2023-03-07 15:16:17 +0600

    render template

--- flaskApplication.py
+++ flaskApplication.py
@@ -1,27 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/math')
-def math():
-    operations = ['add', 'subtract', 'multiply', 'divide', 'modulus']
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    results = []
-    for op in operations:
-        if op == 'add':
-            result = a + b
-        elif op == 'subtract':
-            result = a - b
-        elif op == 'multiply':
-            result = a * b
-        elif op == 'divide':
-            result = a / b
-        elif op == 'modulus':
-            result = a % b
-        else:
-            result = None
-        results.append((op, result))
-    return str(results)
+@app.route('/')
+def home():
+    return render_template('index.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
